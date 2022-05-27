cd5c18adaf8f100991f8eb44c3fcb5b81cf3b54a
houseontherivr <wertyboom911@hotmail.com>
2022-05-27 00:26:24 -0500

    asdf

--- src/GenericsWildCards.java
+++ src/GenericsWildCards.java
@@ -8,6 +8,12 @@ public class GenericsWildCards {
         ints.add(3); ints.add(5); ints.add(10);
         double sum = sum(ints);
         System.out.println("Sum of ints="+sum);
+        ///////////////----^^^upperBound code
+        
+        ///////////////---subtyping with generic wildcards
+        List<? extends Integer> intList = new ArrayList<>();
+        List<? extends Number>  numList = intList;
+        //wildcard parameters CAN be subtyped?
     }
 
     public static double sum(List<? extends Number> list){
@@ -16,5 +22,16 @@ public class GenericsWildCards {
             sum += n.doubleValue();
         }
         return sum;//values inside list don't have to be a Number to work. Can be any child of Number like Integer or Double
+        //upperBound is Number, any type below the upperBound can be used
     }
+    
+    public static void printData(List<?> list){
+      for(Object obj : list){
+          System.out.print(obj + "::");
+      }
+  } //unbound wild card, can be anything, upperBound is Object
+    
+    public static void addIntegers(List<? super Integer> list){
+      list.add(new Integer(50));
+  }//lowerBound, achieved by using super keyword. Any type above integer can be used
 }
